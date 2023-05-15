0fb5c9a516372a0c829cbf04fb3849bbc572d596
aleksandr churilin <alex77bel@yandex.ru>
2023-05-16 02:12:43 +0300

    немного подправил POST

--- simple_web.py
+++ simple_web.py
@@ -14,12 +14,19 @@ class MyServer(BaseHTTPRequestHandler):
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        print(body.decode())
+        body = self.rfile.read(content_length).decode()
+
+        print(body)
+
+        self.send_response(201)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(f"POST body:\n{body}", "utf-8"))
+
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    print(f"Server started http://{hostName}:{serverPort}")
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
